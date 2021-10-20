e72ecd948fc10f3022a880f8effef5399c901626
GSantiago <73957003+GSSantiago@users.noreply.github.com>
2021-10-20 15:44:27 -0300

    Criação do Back-End

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ensureAuthenticated } from "./controllers/middleware/ensureAuthenticated";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+
+const router = Router();
+
+router.post("/authenticate" , new AuthenticateUserController().handle)
+
+router.post("/messages" ,ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile" ,ensureAuthenticated ,new ProfileUserController().handle)
+export {router};
